956876f901c102220bf873a48492adf0f0382675
Ib rahim <ibrahimcse.eng@gmail.com>
2021-09-02 18:42:41 +0600

    Data Structure and Algorithms  Python

---
+++ Data Structure and Algorithms  Python/Linked list insertion.py
@@ -0,0 +1,13 @@
+class Node(object):
+
+    dif __init__(self,data):
+        self.data;
+        self.nextNode = None;
+    
+    class LinkedList(object):
+    
+        def__init__(self):
+            self.head = None;
+            self.size = 0;
+        
+        def 
